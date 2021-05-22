b08010961a2e65f3044dfb7d29539c0b14b8da80
Dan McCrady <dan.mccrady@sectigo.com>
2021-05-21 23:08:36 -0400

    seperating generated clients into folders

---
+++ generate-clients.sh
@@ -0,0 +1,18 @@
+#! /bin/sh
+
+NPM_VERSION=0.0.1
+NPM_DESCRIPTION="Sleeper Api Client that was auto generated from an OpenAPi Specification"
+NPM_AUTHOR=blastdan
+
+TYPSCRIPT_NESTJS_DIR=typescript/nestjs
+TYPSCRIPT_NESTJS_NPMNAME=sleeper-api-nestjs-client
+
+TYPSCRIPT_ANGULAR_DIR=typescript/angular
+TYPSCRIPT_ANGULAR_NPMNAME=sleeper-api-angular-client
+
+openapi-generator-cli generate -i sleeper-openapi.yaml -g typescript-nestjs -o $TYPSCRIPT_NESTJS_DIR -p withInterfaces=true -p npmName=$TYPSCRIPT_NESTJS_NPMNAME -p npmVersion=$NPM_VERSION
+openapi-generator-cli generate -i sleeper-openapi.yaml -g typescript-angular -o $TYPSCRIPT_ANGULAR_DIR -p withInterfaces=true -p npmName=$TYPSCRIPT_ANGULAR_NPMNAME -p npmVersion=$NPM_VERSION
+touch $TYPSCRIPT_NESTJS_DIR/.npmignore
+
+json -I -f ./$TYPSCRIPT_NESTJS_DIR/package.json -e "this.description='$NPM_DESCRIPTION'" -e "this.author='$NPM_AUTHOR'"
+json -I -f ./$TYPSCRIPT_ANGULAR_DIR/package.json -e "this.description='$NPM_DESCRIPTION'" -e "this.author='$NPM_AUTHOR'"
